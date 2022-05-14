24183ed3e8cdbcbac7279bd145af702c055c4ba2
Edgard LS Simas <101891542+Edaygard@users.noreply.github.com>
2022-05-14 16:23:02 -0300

    Calculadora com Forms
    Aula dia 14/05

---
+++ CalculadoraForms/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CalculadoraForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btSoma = new System.Windows.Forms.Button();
+            this.btSubtrai = new System.Windows.Forms.Button();
+            this.btMulti = new System.Windows.Forms.Button();
+            this.btDiv = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o 1º número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Digite o 2º número:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Calculadora em Forms";
+            // 
+            // btSoma
+            // 
+            this.btSoma.Location = new System.Drawing.Point(371, 34);
+            this.btSoma.Name = "btSoma";
+            this.btSoma.Size = new System.Drawing.Size(103, 24);
+            this.btSoma.TabIndex = 6;
+            this.btSoma.Text = "SOMA";
+            this.btSoma.UseVisualStyleBackColor = true;
+            this.btSoma.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btSubtrai
+            // 
+            this.btSubtrai.Location = new System.Drawing.Point(371, 77);
+            this.btSubtrai.Name = "btSubtrai";
+            this.btSubtrai.Size = new System.Drawing.Size(103, 24);
+            this.btSubtrai.TabIndex = 7;
+            this.btSubtrai.Text = "SUBTRAÇÃO";
+            this.btSubtrai.UseVisualStyleBackColor = true;
+            this.btSubtrai.Click += new System.EventHandler(this.btSubtrai_Click);
+            // 
+            // btMulti
+            // 
+            this.btMulti.Location = new System.Drawing.Point(371, 118);
+            this.btMulti.Name = "btMulti";
+            this.btMulti.Size = new System.Drawing.Size(103, 24);
+            this.btMulti.TabIndex = 8;
+            this.btMulti.Text = "MULTIPLICAÇÃO";
+            this.btMulti.UseVisualStyleBackColor = true;
+            this.btMulti.Click += new System.EventHandler(this.btMulti_Click);
+            // 
+            // btDiv
+            // 
+            this.btDiv.Location = new System.Drawing.Point(371, 159);
+            this.btDiv.Name = "btDiv";
+            this.btDiv.Size = new System.Drawing.Size(103, 24);
+            this.btDiv.TabIndex = 9;
+            this.btDiv.Text = "DIVISÃO";
+            this.btDiv.UseVisualStyleBackColor = true;
+            this.btDiv.Click += new System.EventHandler(this.btDiv_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Resultado";
+            // 
+            // Resultado
+            // 
+            this.Resultado.Location = new System.Drawing.Point(202, 148);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(116, 20);
+            this.Resultado.TabIndex = 11;
+            this.Resultado.TextChanged += new System.EventHandler(this.Resultado_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(341, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Escolha uma Operação:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 218);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btDiv);
+            this.Controls.Add(this.btMulti);
+            this.Controls.Add(this.btSubtrai);
+            this.Controls.Add(this.btSoma);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btSoma;
+        private System.Windows.Forms.Button btSubtrai;
+        private System.Windows.Forms.Button btMulti;
+        private System.Windows.Forms.Button btDiv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CalculadoraForms/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float valor1, valor2, resultado;
+
+            valor1 = float.Parse(textBox1.Text);
+            valor2 = float.Parse(textBox2.Text);
+
+            resultado = valor1 + valor2;
+
+            Resultado.Text = resultado.ToString();
+        }
+
+        private void btSubtrai_Click(object sender, EventArgs e)
+        {
+            decimal valor1, valor2, resultado;
+
+            valor1 = Convert.ToDecimal(textBox1.Text);
+            valor2 = Convert.ToDecimal(textBox2.Text);
+
+            resultado = valor1 - valor2;
+
+            Resultado.Text = resultado.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMulti_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, resultado;
+
+            valor1 = Convert.ToDouble(textBox1.Text);
+            valor2 = Convert.ToDouble(textBox2.Text);
+
+            resultado = valor1 * valor2;
+
+            Resultado.Text = resultado.ToString();
+        }
+
+        private void btDiv_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, resultado;
+
+            valor1 = Convert.ToDouble(textBox1.Text);
+            valor2 = Convert.ToDouble(textBox2.Text);
+
+           if(valor2 == 0)
+            {
+                resultado = 0;
+            }
+           else
+            resultado = valor1 / valor2;
+
+            Resultado.Text = resultado.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
